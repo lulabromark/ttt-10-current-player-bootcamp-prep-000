8ab4fbea525dbf1a00bc95f562a9f95314b8f577
Mark Lawrence <lawrence.ml@gmail.com>
2018-05-16 02:31:00 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,15 +1,13 @@
 def turn_count(board)
   counter = 0
   board.each do |turn|
-    if turn == "X" || turn == "O"
-      counter += 1
-    end
+    counter += 1 if turn == 'X' || turn == 'O'
   end
-  return counter
+  counter
 end
 
 def current_player(board)
   counter = turn_count(board)
-  
-  counter.even? ? "X" : "O"
+
+  counter.even? ? 'X' : 'O'
 end
